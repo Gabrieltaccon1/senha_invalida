75b10aa3ba9a607c4226cf1176aa454ba37a1e47
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 14:38:30 -0300

    uri 1114

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uri1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+            string nome;
+
+            Console.WriteLine("DIGITE SEU NOME");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("DIGITE SUA SENHA");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Olá," + nome + " SENHA INVALIDA TENTE NOVAMENTE");
+                senha = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("OLÁ " + nome + " SENHA CORRETA");
+            Console.ReadLine();
+
+        }
+    }
+}
